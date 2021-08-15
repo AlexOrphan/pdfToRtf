6d97a84b55bcbcfdacb0da99c2b3c01bc808c0f1
Alex Sirotin <alexander.sirotin@gmail.com>
2021-08-15 13:15:58 +0300

    autoload classes

--- convertPdf.php
+++ convertPdf.php
@@ -1,8 +1,17 @@
 #!/usr/bin/env php
 <?php
-// application.php
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
+
+ini_set("log_errors", 1);
+ini_set("error_log", __DIR__  . "/logs/php-error.log");
+
+spl_autoload_register(function ($class) {
+    $file = __DIR__ .'/'. str_replace('\\', '/', $class) .'.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+});
 
 use Symfony\Component\Console\Application;
 use App\Command\ConvertPdfCommand;
